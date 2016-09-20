93cc4c4c69cbaa3cb1de4545b0b0cdd992be0506
eternity6170 <eternity6170@gmail.com>
2016-09-21 00:20:06 +0900

    세 줄로 타일 깔기

---
+++ src/jungol/q2112/Main.java
@@ -0,0 +1,30 @@
+package jungol.q2112;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Created by hope on 2016. 9. 20..
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+        int N = S.nextInt();
+
+        int[] DP = new int[N+1];
+        int sum = 0;
+
+        DP[0] = 1;
+        DP[1] = 0;
+
+        for(int i = 2; i <= N; i += 2) {
+            DP[i] = DP[i - 2] * 3 + sum * 2;
+            sum += DP[i - 2];
+
+            //System.out.println(i + " : " + Arrays.toString(DP));
+        }
+
+        System.out.println(DP[N]);
+
+    }
+}
